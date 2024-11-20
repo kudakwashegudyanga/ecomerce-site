eaab3602015df24071c657e212117e988e9090ff
kudakwashe gudyanga <kudakwashegudyanga@41gmail.com>
2024-11-20 15:07:25 +0200

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,17 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+// 	baseURL: import.meta.mode === "development" ? "https://ecomerce-site-ml5t.onrender.com" : "/api",
+// 	withCredentials: true, // send cookies to the server
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://ecomerce-site-ml5t.onrender.com" : "/api",
-	withCredentials: true, // send cookies to the server
+    baseURL: import.meta.env.MODE === "development" ? "https://ecomerce-site-ml5t.onrender.com" : "https://api.my-ecommerce-site.com", // Correct production URL
+    withCredentials: true,
 });
 
 export default axiosInstance;
