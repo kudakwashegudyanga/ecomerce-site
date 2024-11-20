b26519b7635ff16cc0f589e86e80864bccf5f24b
kudakwashe gudyanga <kudakwashegudyanga@41gmail.com>
2024-11-20 15:01:58 +0200

    cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -15,6 +16,7 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
