428d5c422df96d803e371457b87e34e6646d6309
kudakwashe gudyanga <kudakwashegudyanga@41gmail.com>
2024-11-20 15:28:31 +0200

    done123

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,17 +1,8 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-// 	baseURL: import.meta.mode === "development" ? "https://ecomerce-site-ml5t.onrender.com" : "/api",
-// 	withCredentials: true, // send cookies to the server
-// });
-
-// export default axiosInstance;
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://ecomerce-site-ml5t.onrender.com" : "/api", // Correct production URL
-    withCredentials: true,
+	baseURL: import.meta.mode === "development" ? "hhttps://ecomerce-site-ml5t.onrender.com" : "/api",
+	withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
