195371bfdbc27d1ed53c1432bc85bbfc80eb950f
kudakwashe gudyanga <kudakwashegudyanga@41gmail.com>
2024-11-20 14:55:47 +0200

    roro

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "hhttps://ecomerce-site-ml5t.onrender.com" : "/api",
+	baseURL: import.meta.mode === "development" ? "https://ecomerce-site-ml5t.onrender.com" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
